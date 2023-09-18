c07e025bf74171d08aa53c113686ffc79f378400
Andrey_Abramov <i.andrei.abramov@gmail.com>
2023-09-18 19:00:02 +0300

    feat: adds and configures styled-components

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,15 +2,19 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import PageDashboard from 'pages/Dashboard';
 import PageHome from 'pages/Home';
+import { ThemeProvider } from 'styled-components';
+import { darkTheme } from 'styles/themes';
 
 import { GetRoute } from 'utils/routes/get-route';
 
 const App: React.FC = () => {
   return (
-    <Routes>
-      <Route path={GetRoute.Home()} element={<PageHome />} />
-      <Route path={GetRoute.Dashboard()} element={<PageDashboard />} />
-    </Routes>
+    <ThemeProvider theme={darkTheme}>
+      <Routes>
+        <Route path={GetRoute.Home()} element={<PageHome />} />
+        <Route path={GetRoute.Dashboard()} element={<PageDashboard />} />
+      </Routes>
+    </ThemeProvider>
   );
 };
 
