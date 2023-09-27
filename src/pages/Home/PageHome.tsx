851129c7e267e2c06fa8ab63815038f35488909a
Andrey_Abramov <i.andrei.abramov@gmail.com>
2023-09-27 21:57:23 +0300

    feat: adds redirect from index page to login page

--- src/pages/Home/PageHome.tsx
+++ src/pages/Home/PageHome.tsx
@@ -1,15 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 
 import { GetRoute } from 'utils/routes/get-route';
 
 const PageHome: React.FC = () => {
-  return (
-    <div>
-      <h2>PageHome</h2>
-      <Link to={GetRoute.Dashboard()}>To Dashboard</Link>
-    </div>
-  );
+  return <Navigate to={GetRoute.Login()} />;
 };
 
 export default PageHome;
